2319056fbe97b2149fcc467495883f1b76df53cd
Prem Morya <premmorya1996@gmail.com>
2026-07-21 16:38:22 +0530

    fixed robot ts file

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,12 @@
-import { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
+      userAgent: "*",
+      allow: "/",
     },
-    sitemap: 'https://invoicezy.com/sitemap.xml',
-  }
+    sitemap: "https://invoicezy.com/sitemap.xml",
+    host: "https://invoicezy.com",
+  };
 }
